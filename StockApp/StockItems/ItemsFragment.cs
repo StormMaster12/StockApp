16fa74e9bb5e303da641068fad9374795349355d
StormMaster12 <james.scotland@yahoo.co.uk>
2017-09-06 09:12:36 +0100

    Created ItemsFragment

---
+++ StockApp/StockItems/ItemsFragment.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Gms.Vision.Barcodes;
+using Android.Gms.Common.Apis;
+
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+using Android.Support.V7.App;
+
+namespace StockApp.StockItems
+{
+}
