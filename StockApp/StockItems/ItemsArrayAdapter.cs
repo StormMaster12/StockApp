df748eb31d94b05e1e4cc808378e12b1482a6c8b
StormMaster12 <james.scotland@yahoo.co.uk>
2017-09-13 11:12:02 +0100

    Update ItemsArrayAdapter.cs

--- StockApp/StockItems/ItemsArrayAdapter.cs
+++ StockApp/StockItems/ItemsArrayAdapter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +14,10 @@ using StockApp.HTTP;
 
 namespace StockApp.StockItems
 {
-    class ItemsArrayAdapter : ArrayAdapter<RootJson>, IActivityResponse
+    class ItemsArrayAdapter : ArrayAdapter<RootJson>
     {
         private Context mContext { get; set; }
         private List<RootJson> ItemsList { get; set; }
-        private HttpPost getHttp = new HttpPost();
-        private string[] strHttp = new string[8];
-        private int pos;
 
         public ItemsArrayAdapter(Context context, List<RootJson> list): base(context, Resource.Layout.StockItem_Fragment)
         {
@@ -31,7 +28,6 @@ namespace StockApp.StockItems
 
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
-            pos = position;
             LayoutInflater inflater = (LayoutInflater)mContext.GetSystemService(Context.LayoutInflaterService);
 
             View rowView = inflater.Inflate(Resource.Layout.StockItem_Fragment, parent, false);
@@ -39,15 +35,9 @@ namespace StockApp.StockItems
             ImageView imageView = (ImageView)rowView.FindViewById(Resource.Id.ItemImage);
             Button itemButton = (Button)rowView.FindViewById(Resource.Id.ItemButton);
 
-
-            Console.WriteLine(position);
-
             RootJson item =  GetItem(position);
             if (item != null)
             {
-
-           
-
                 textView.Text = "Name: " + item.Name + "\n Amount: " +  item.Amount + "\n Expiry Date: " + item.expiryDate;
                 //imageView.SetImageURI(new Android.Net.Uri("http://google.com"));
                 itemButton.Text = "Click To see more information about :" + item.Name;
@@ -56,30 +46,26 @@ namespace StockApp.StockItems
             return rowView;
         }
 
-        private void ItemButton_Click(object sender, EventArgs e)
+        private void ItemButton_Click(AdapterView<RootJson> adapter, View v, int position, long id)
         {
+			string[] strHttp = new string[8];
             strHttp[0] = "http://ec2-35-177-33-89.eu-west-2.compute.amazonaws.com/";
             strHttp[1] = "getSpecific";
-            strHttp[2] = GetItem(pos).Name;
-            strHttp[3] = GetItem(pos).Pan;
-            strHttp[4] = GetItem(pos).Amount;
-            strHttp[5] = GetItem(pos).expiryDate;
+            strHttp[2] = GetItem(position).Name;
+            strHttp[3] = GetItem(position).Pan;
+            strHttp[4] = GetItem(position).Amount;
+            strHttp[5] = GetItem(position).expiryDate;
 
+			Intent intent = new Intent(mContext, typeof(ItemsDetailedActivity));
+            intent.PutExtra("getSpecificList", strHttp);
+			mContext.startActivity(intent);
+			
             //strPost += "Name= " + strParams[2] + "&";
             //strPost += "Pan= " + strParams[3] + "&";
             //strPost += "Amount= " + strParams[4] + "&";
             //strPost += "expiryDate= " + strParams[5] + "&";
 
-
-            getHttp.Execute(strHttp);
             Toast.MakeText(mContext, "Not Implemented Yet", ToastLength.Short).Show();
-        }
-
-        public void proccessFinish(List<RootJson> jsonList)
-        {
-            Intent intent = new Intent(mContext, typeof(ItemsDetailedActivity));
-            //intent.PutExtra("detailedList", jsonList);
-            
         }
     }
 }
