87faf7e550b3c4a5e3a1ee9dd3d6361c91af1708
StormMaster12 <james.scotland@yahoo.co.uk>
2017-09-13 11:23:39 +0100

    Moved HttpPost into the loading of the activity

--- StockApp/StockItems/ItemsDetailedActivity.cs
+++ StockApp/StockItems/ItemsDetailedActivity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+?using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,32 +15,48 @@ using StockApp.HTTP;
 namespace StockApp.StockItems
 {
     [Activity(Label = "Detailed Item Activity")]
-    class ItemsDetailedActivity : AppCompatActivity
+    class ItemsDetailedActivity : AppCompatActivity, IActivityResponse
     {
         private List<RootJson> detailedList;
+		private HttpPost httpPost = new HttpPost();
+		private TextView itemName;
+		private TextView itemStatistics;
+		private TextView itemDescription;
+		private ImageView itemImage;
+		
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.StockItem_Detailed);
 
             Intent intent = Intent;
-            detailedList =  intent.GetSerializableExtra("detailedList") as List<RootJson>;
+            strHttpList =  intent.GetSerializableExtra("getSpecificList") as string[];
 
-            TextView itemName = FindViewById(Resource.Id.DetailedItemName) as TextView;
-            TextView itemStatistics = FindViewById(Resource.Id.DetailedItemStatistics) as TextView;
-            TextView itemDescription = FindViewById(Resource.Id.DetailedItemDescription) as TextView;
-            ImageView itemImage = FindViewById(Resource.Id.DetailedItemImage) as ImageView;
-
-            RootJson item = detailedList[0];
-
-            itemName.Text = item.Name;
-            itemStatistics.Text = "Date Purchased : " + item.purchaseDate + "\n Expiry Date: " + item.expiryDate + "\n Amount Left: " + item.Amount + "\n Pan: " + item.Pan;
-            itemDescription.Text = "Short Description:\n " + item.Short_Description + "\n Long Description:\n " + item.Long_Description;
+            itemName = FindViewById(Resource.Id.DetailedItemName) as TextView;
+            itemStatistics = FindViewById(Resource.Id.DetailedItemStatistics) as TextView;
+            itemDescription = FindViewById(Resource.Id.DetailedItemDescription) as TextView;
+            itemImage = FindViewById(Resource.Id.DetailedItemImage) as ImageView;
+	
+			httpPost.activityResponse = this;
+			
+			httpPost.Execute(strHttpList);
         }
 
         public ItemsDetailedActivity()
         {
 
+        }
+		
+		public void proccessFinish(List<RootJson> jsonList)
+        {
+            RootJson item = jsonList[0];
+
+            itemName.Text = item.Name;
+            itemStatistics.Text = "Date Purchased : " + item.purchaseDate + "\n Expiry Date: " + item.expiryDate + "\n Amount Left: " + item.Amount + "\n Pan: " + item.Pan;
+            itemDescription.Text = "Short Description:\n " + item.Short_Description + "\n Long Description:\n " + item.Long_Description;
+			
+			httpPost.Cancel(true);
+            
         }
     }
 }
